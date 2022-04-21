5b323208f451516a63d3143e7ee774d81922ec6b
Rahul Biswas <104161953+Rahulbiswas9502@users.noreply.github.com>
2022-04-22 00:54:46 +0600

    Update DistinctYear.py

--- DistinctYear.py
+++ DistinctYear.py
@@ -1,1 +1,16 @@
+x=input("")
+for i in range(9000) :
+
+    x=int(x)
+    x = x + 1
+    x=x.__str__()
+    a = (x[0])
+    b = (x[1])
+    c = (x[2])
+    d = (x[3])
+    if  a!=b and a!=c and a!=d and b!=c and b!=d and c!=d:
+        break
+    else:
+       continue
+print(x)
 
